85e440240741eda365a217edc5638464d164692e
Santiago Ruano <ivansantiagoruanoruiz3@gmail.com>
2024-09-26 16:48:25 -0500

    Se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,25 @@
+#ejemplos de operadores de asignacion
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+
+var1 += 3
+print(f"Asignacion con suma: {var1}")
+
+var2 -= 3
+print(f"Asignacion con resta: {var2}")
+
+var3 *= 3
+print(f"Asignacion con multiplicacion: {var3}")
+
+var4 /= 3
+print(f"Asignacion con division: {var4}")
+
+var5 %= 3
+print(f"Asignacion con residuo: {var5}")
+var6 //= 3
+print(f"Asignacion con division entera: {var6}")
---
+++ operadoresProject/OperadoresLogicos.py
@@ -0,0 +1,6 @@
+q = 5
+print(q > 4 and q < 9)
+
+print(q > 4 or q < 10)
+
+
